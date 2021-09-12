7b7c3a14be66679ff4b5bb6fc43cfb875ce87741
brunoalfred <alfredybruno@gmail.com>
2021-09-12 17:53:40 +0300

    helpers in the tests suit

---
+++ message_chat/test/helpers.dart
@@ -0,0 +1,17 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:rethink_db_ns/rethink_db_ns.dart';
+
+Future<void> createDb(RethinkDb r, Connection connection) async {
+  await r.dbCreate('test').run(connection).catchError((err) => {});
+  await r.tableCreate('users').run(connection).catchError((err) => {});
+  await r.tableCreate('messages').run(connection).catchError((err) => {});
+  await r.tableCreate('receipts').run(connection).catchError((err) => {});
+  await r.tableCreate('typing_events').run(connection).catchError((err) => {});
+}
+
+Future<void> cleanDb(RethinkDb r, Connection connection) async {
+  await r.table('users').delete().run(connection);
+  await r.table('messages').delete().run(connection);
+  await r.table('receipts').delete().run(connection);
+  await r.table('typing_events').delete().run(connection);
+}
